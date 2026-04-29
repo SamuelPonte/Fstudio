09eed8799f71998d8118730cc5132b07250eddf8
Samuel Ponte <samuel.ramosponte@gmail.com>
2026-04-29 13:29:15 +0100

    criação inicial da aplicação web

---
+++ Fstudio/Fstudio/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fstudio.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
